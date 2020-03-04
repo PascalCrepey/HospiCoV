a9a123dcff84da9a2a962fcf771fc7e93d8cb40f
Pascal Crépey <crepey@gmail.com>
2020-03-04 20:38:09 +0100

    - leaflet

--- R/TestVoronoi.R
+++ R/TestVoronoi.R
@@ -6,20 +6,13 @@ library(data.table)
 library(stringr)
 library(raster)
 library(maps)
+library(leaflet)
 
 # functions computing the voronoi tesselisations
-
-France = get_map(c(left = -5, top = 51.3, right = 8.5, bottom = 41.7), 
-                 source = "google")
-
  
 outlineFr = map_data(map = "world", region = "france")
 
-
-ggmap(France)
-
 #get the data
-
 hospCovid = data.table(read.xlsx("Data/Hopitaux_covid.xlsx"))
 hospCovid[, FINESS_GEO := as.character(FINESS_GEO)]
 hospCovid[, FINESS_GEO := str_pad(FINESS_GEO, 9 , pad = "0")]
@@ -35,12 +28,14 @@ mHBase = mHBase[!(is.na(FINESS_GEO) | is.na(finess_EJ) | is.na(lat))]
 
 vorPolyHosp = voronoi_polygon(mHBase, x = "lng", y = "lat", outline = outlineFr)
 
-ggmap(France) + geom_path(data = fortify_voronoi(vorPolyHosp), 
-                          )
+vorPolyHosp %>%
+  leaflet() %>%
+  addTiles() %>%
+  addPolylines() %>%
+  addCircleMarkers(lng = mHBase$lng, 
+                   lat = mHBase$lat, 
+                   opacity = 0.8, 
+                   radius = 2, 
+                   color = "red")
   
-ggmap(France, base_layer = ggplot(data = mHBase,aes(x = lng, y = lat))) +
-  stat_voronoi( geom = "path" , outline = outlineFr, color = "blue") +
-  geom_point(size = 1, color = "red") 
-    
-  
-  geom_path(data = fortify_voronoi(vorPolyHosp), aes(x,y))
+
