957d897e97245f0edfedd8099fde8bb3b9ae11c0
ClementMassonnaud <clement.massonnaud@gmail.com>
2020-03-10 15:50:47 +0100

    modified shiny layout

--- R/mod_model.R
+++ R/mod_model.R
@@ -15,87 +15,89 @@
 #' @importFrom shiny NS tagList 
 mod_model_ui <- function(id){
     ns <- NS(id)
-
-    input_byage = function(id) {
-        tagList(
-            numericInput(ns(paste0(id,"0-4")), label = "0-4", value = 0),
-            numericInput(ns(paste0(id,"5-9")), label = "5-9", value = 0),
-            numericInput(ns(paste0(id,"10-14")), label = "10-14", value = 0),
-            numericInput(ns(paste0(id,"15-19")), label = "15-19", value = 0),
-            numericInput(ns(paste0(id,"20-24")), label = "20-24", value = 0),
-            numericInput(ns(paste0(id,"25-29")), label = "25-29", value = 0),
-            numericInput(ns(paste0(id,"30-34")), label = "30-34", value = 0),
-            numericInput(ns(paste0(id,"35-39")), label = "35-39", value = 0),
-            numericInput(ns(paste0(id,"40-44")), label = "40-44", value = 0),
-            numericInput(ns(paste0(id,"45-49")), label = "45-49", value = 0),
-            numericInput(ns(paste0(id,"50-54")), label = "50-54", value = 0),
-            numericInput(ns(paste0(id,"55-59")), label = "55-59", value = 0),
-            numericInput(ns(paste0(id,"60-64")), label = "60-64", value = 0),
-            numericInput(ns(paste0(id,"65-69")), label = "65-69", value = 0),
-            numericInput(ns(paste0(id,"70-74")), label = "70-74", value = 0),
-            numericInput(ns(paste0(id,"75-79")), label = "75-79", value = 0),
-            numericInput(ns(paste0(id,"80P")), label = "80P", value = 0)
-        )
-    }
-    
     
   agenames = colnames(contact_matrix)
-  tagList(
-      sidebarLayout(
-          sidebarPanel(
-              h4("Model parameters"),
-              tabsetPanel(
-                  tabPanel("Epidemic params.", 
-                           uiOutput(ns("paramsEpiUI"))),
-                  tabPanel("Population params.",
-                           uiOutput(ns("paramsPopUI")))
-              ),
-              h4("Outcomes"),
-              tabsetPanel(
-                  tabPanel("Severity",
-                           "Input risk of being severe (from 0 to 1), for each age group",
-                           input_byage("severity")),
-                  tabPanel("ICU hospit",
-                           "Input risk of being admitted to ICU (from 0 to 1), for each age group",
-                           input_byage("ICU"))
-              ),                         
-              width = 3
-          ),
-          mainPanel(
-            fluidRow(column(4, selectInput(inputId = ns("selectedAG"), label = NULL,
-                                           choices = c("All", "Aggregated", 
-                                                       agenames), selected = "All")),
-                     column(4, selectInput(inputId = ns("selectedOutcome"), 
-                                           label = NULL,
-                                           choices = c("Infected", 
-                                                         "Symptomatic cases", 
-                                                         "Mild cases", 
-                                                         "Hospitalized cases", 
-                                                         "ICU cases", 
-                                                         "ICU with O2"), 
-                                          selected = "Infected")),
-                     column(4, selectInput(inputId = ns("selectedDuration"),
-                                           label = NULL,
-                                           choices = c("Week", "Month", "Trimester", 
-                                                       "Semester", "Year"),
-                                           selected = "Trimester"
-                                           ))),
-            fluidRow(column(12,
-                            plotly::plotlyOutput(ns("mainPlot"))
-                            )
-                     ),
-            fluidRow(column(12,
-                            tabsetPanel(
-                                tabPanel("Severity",
-                                         uiOutput(ns("dateRangeInput")),
-                                         plotly::plotlyOutput(ns("severity"))
-                                         ),
-                                tabPanel("ICU Hospit"))
-                            )
-                     )
-          )
-      )
-  )
+    tagList(
+        tabsetPanel(
+            tabPanel(
+                title = "Epidemic curves",
+                sidebarLayout(
+                    sidebarPanel(
+                        h4("Model parameters"),
+                        tabsetPanel(
+                            tabPanel("Epidemic params.", 
+                                     uiOutput(ns("paramsEpiUI"))),
+                            tabPanel("Population params.",
+                                     uiOutput(ns("paramsPopUI")))
+                        )
+                    ),
+                    mainPanel(
+                        fluidRow(column(4, selectInput(inputId = ns("selectedAG"), label = NULL,
+                                                       choices = c("All", "Aggregated", 
+                                                                   agenames), selected = "All")),
+                                 column(4, selectInput(inputId = ns("selectedOutcome"), 
+                                                       label = NULL,
+                                                       choices = c("Infected", 
+                                                                   "Symptomatic cases", 
+                                                                   "Mild cases", 
+                                                                   "Hospitalized cases", 
+                                                                   "ICU cases", 
+                                                                   "ICU with O2"), 
+                                                       selected = "Infected")),
+                                 column(4, selectInput(inputId = ns("selectedDuration"),
+                                                       label = NULL,
+                                                       choices = c("Week", "Month", "Trimester", 
+                                                                   "Semester", "Year"),
+                                                       selected = "Trimester"
+                                                       ))),
+                        fluidRow(column(12,
+                                        plotly::plotlyOutput(ns("mainPlot"))
+                                        )
+                                 )
+                    )
+                )
+            ),
+            tabPanel(
+                title = "Outcomes",
+                sidebarLayout(
+                    sidebarPanel(
+                        h4("Outcomes"),
+                        tabsetPanel(
+                            tabPanel(
+                                "Severity",
+                                HTML(
+                                    "Input risk of being severe (from 0 to 1), for each age group",
+                                    "</br>",
+                                    "Source:"),
+                                a("Guan et al.", href = "https://www.nejm.org/doi/full/10.1056/NEJMoa2002032", target = "_blank"),
+                                DT::DTOutput(ns("severity_risk"))
+                            ),
+                            tabPanel(
+                                "ICU hospit",
+                                "Input risk of being admitted to ICU (from 0 to 1), for each age group",
+                                        #                           input_byage("ICU"))
+                                )
+                        ),
+                        width = 3
+                    ),
+                    mainPanel(
+                        fluidRow(column(10,
+                                        uiOutput(ns("dateRangeInput")),
+                                        plotly::plotlyOutput(ns("outcome"))
+                                        ),
+                                 column(2,
+                                        radioButtons(ns("outcomeType"),
+                                                     label = "Outcome",
+                                                     choices = c("Severity"       = "severity",
+                                                                 "ICU admissions" = "ICU")
+                                                     )
+                                        )
+                                 )
+                    )
+                )
+            )
+        )
+    )
 }
     
 # Module Server
@@ -221,35 +223,25 @@ mod_model_server <- function(input, output, session){
                   value = c(min, max))
       )
   })
+
+  severity_risk_table = DT::datatable(severity_risk,
+                                      selection = list(target = "column", mode = "single"),
+                                      options   = list(pageLength = 17),
+                                      editable  = list(target = "column",
+                                                      disable = list(columns = 1))
+                                      )
+
+  output$severity_risk = DT::renderDT({ severity_risk_table })
   
-  severity_risk_byage = reactive({
-      c(input$`severity0-4`,
-        input$`severity5-9`,
-        input$`severity10-14`,
-        input$`severity15-19`,
-        input$`severity20-24`,
-        input$`severity25-29`,
-        input$`severity30-34`,
-        input$`severity35-39`,
-        input$`severity40-44`,
-        input$`severity45-49`,
-        input$`severity50-54`,
-        input$`severity55-59`,
-        input$`severity60-64`,
-        input$`severity65-69`,
-        input$`severity70-74`,
-        input$`severity75-79`,
-        input$`severity80P`)
-  })
-
   severity_table = reactive({
-      severity_byage(simulation(), risk_byage = severity_risk_byage())
+      severity_byage(simulation(), severity_risk)
   })
 
-  output$severity = plotly::renderPlotly({
-      severity_barchart(severity_table(),
-                        start_time = input$dateRange[[1]],
-                        end_time = input$dateRange[[2]])
+  output$outcome = plotly::renderPlotly({
+      req(input$dateRange)
+      outcome_barchart(severity_table(),
+                       start_time = input$dateRange[[1]],
+                       end_time = input$dateRange[[2]])
   })
 
   ## ----- END OF OUTCOMES ----------------------------------------------
@@ -262,3 +254,4 @@ mod_model_server <- function(input, output, session){
 ## To be copied in the server
 # callModule(mod_model_server, "model_ui_1")
  
+## TODO OUTCOME ICU
