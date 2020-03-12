f7cd1c78367119e8920bf772edc7d0c88356991a
ROUX <jonathan.roux@ehesp.fr>
2020-03-12 14:52:42 +0100

    Modify renderCurves to take into account several regions at the same time

--- R/render-curves.R
+++ R/render-curves.R
@@ -2,10 +2,11 @@
 renderCurves <- function(data, outcome, selectedAG) {
     ## --- INFECTED CURVES -------------------------------------------------------
     if (outcome == "Infected") {
-        
+    
         mainPlot = renderPlotly({
             if (selectedAG == "All") {
-                p = ggplot(data, aes(x = Time, y = Infected, color = AgeGroup)) +
+                dataAgg = data[, .(Infected = sum(Infected)), by = c("Time","AgeGroup")]
+                p = ggplot(dataAgg, aes(x = Time, y = Infected, color = AgeGroup)) +
                     theme_classic() +
                     geom_line()
             } else if (selectedAG == "Aggregated") {
@@ -15,7 +16,8 @@ renderCurves <- function(data, outcome, selectedAG) {
                     theme_classic() +
                     geom_line()
             } else{
-                p = ggplot(data[AgeGroup == selectedAG,], aes(x = Time, y = Infected, color = AgeGroup)) +
+                dataAgg = data[AgeGroup == selectedAG, .(Infected = sum(Infected)), by = c("Time","AgeGroup")]
+                p = ggplot(dataAgg, aes(x = Time, y = Infected, color = AgeGroup)) +
                     theme_classic() +
                     geom_line()
             }
@@ -26,7 +28,8 @@ renderCurves <- function(data, outcome, selectedAG) {
         #to be updated
         mainPlot = renderPlotly({
             if (selectedAG == "All") {
-                p = ggplot(data, aes(x = Time, y = Infected, color = AgeGroup)) +
+                dataAgg = data[, .(Infected = sum(Infected)), by = c("Time","AgeGroup")]
+                p = ggplot(dataAgg, aes(x = Time, y = Infected, color = AgeGroup)) +
                     theme_classic() +
                     geom_line()
             } else if (selectedAG == "Aggregated") {
@@ -36,7 +39,8 @@ renderCurves <- function(data, outcome, selectedAG) {
                     theme_classic() +
                     geom_line()
             } else{
-                p = ggplot(data[AgeGroup == selectedAG,], aes(x = Time, y = Infected, color = AgeGroup)) +
+                dataAgg = data[AgeGroup == selectedAG, .(Infected = sum(Infected)), by = c("Time","AgeGroup")]
+                p = ggplot(dataAgg, aes(x = Time, y = Infected, color = AgeGroup)) +
                     theme_classic() +
                     geom_line()
             }
@@ -48,7 +52,8 @@ renderCurves <- function(data, outcome, selectedAG) {
         
         mainPlot = renderPlotly({
             if (selectedAG == "All") {
-                p = ggplot(data, aes(x = Time, y = severe, color = AgeGroup)) +
+                dataAgg = data[, .(severe = sum(severe)), by = c("Time","AgeGroup")]
+                p = ggplot(dataAgg, aes(x = Time, y = severe, color = AgeGroup)) +
                     theme_classic() +
                     geom_line()
             } else if (selectedAG == "Aggregated") {
@@ -58,7 +63,8 @@ renderCurves <- function(data, outcome, selectedAG) {
                     theme_classic() +
                     geom_line()
             } else {
-                p = ggplot(data[AgeGroup == selectedAG,],
+                dataAgg = data[AgeGroup == selectedAG, .(severe = sum(severe)), by = c("Time","AgeGroup")]
+                p = ggplot(dataAgg,
                            aes(x = Time, y = severe, color = AgeGroup)) +
                     theme_classic() +
                     geom_line()
@@ -67,7 +73,8 @@ renderCurves <- function(data, outcome, selectedAG) {
         })
         secondPlot = renderPlotly({
             if (selectedAG == "All") {
-                p = ggplot(data, aes(x = Time, y = non.severe, color = AgeGroup)) +
+                dataAgg = data[, .(non.severe = sum(non.severe)), by = c("Time","AgeGroup")]
+                p = ggplot(dataAgg, aes(x = Time, y = non.severe, color = AgeGroup)) +
                     theme_classic() +
                     geom_line()
             } else if (selectedAG == "Aggregated") {
@@ -77,7 +84,8 @@ renderCurves <- function(data, outcome, selectedAG) {
                     theme_classic() +
                     geom_line()
             } else {
-                p = ggplot(data[AgeGroup == selectedAG,],
+                dataAgg = data[AgeGroup == selectedAG, .(non.severe = sum(non.severe)), by = c("Time","AgeGroup")]
+                p = ggplot(dataAgg,
                            aes(x = Time, y = non.severe, color = AgeGroup)) +
                     theme_classic() +
                     geom_line()
@@ -88,7 +96,8 @@ renderCurves <- function(data, outcome, selectedAG) {
     } else if (outcome == "ICU") {
         mainPlot = renderPlotly({
             if (selectedAG == "All") {
-                p = ggplot(data, aes(x = Time, y = ICU, color = AgeGroup)) +
+                dataAgg = data[, .(ICU = sum(ICU)), by = c("Time","AgeGroup")]
+                p = ggplot(dataAgg, aes(x = Time, y = ICU, color = AgeGroup)) +
                     theme_classic() +
                     geom_line()
             } else if (selectedAG == "Aggregated") {
@@ -98,7 +107,8 @@ renderCurves <- function(data, outcome, selectedAG) {
                     theme_classic() +
                     geom_line()
             } else {
-                p = ggplot(data[AgeGroup == selectedAG,],
+                dataAgg = data[AgeGroup == selectedAG, .(ICU = sum(ICU)), by = c("Time","AgeGroup")]
+                p = ggplot(dataAgg,
                            aes(x = Time, y = ICU, color = AgeGroup)) +
                     theme_classic() +
                     geom_line()
@@ -107,7 +117,8 @@ renderCurves <- function(data, outcome, selectedAG) {
         })
         secondPlot = renderPlotly({
             if (selectedAG == "All") {
-                p = ggplot(data, aes(x = Time, y = non.ICU, color = AgeGroup)) +
+                dataAgg = data[, .(non.ICU = sum(non.ICU)), by = c("Time","AgeGroup")]
+                p = ggplot(dataAgg, aes(x = Time, y = non.ICU, color = AgeGroup)) +
                     theme_classic() +
                     geom_line()
             } else if (selectedAG == "Aggregated") {
@@ -117,7 +128,8 @@ renderCurves <- function(data, outcome, selectedAG) {
                     theme_classic() +
                     geom_line()
             } else {
-                p = ggplot(data[AgeGroup == selectedAG,],
+                dataAgg = data[AgeGroup == selectedAG, .(non.ICU = sum(non.ICU)), by = c("Time","AgeGroup")]
+                p = ggplot(dataAgg,
                            aes(x = Time, y = non.ICU, color = AgeGroup)) +
                     theme_classic() +
                     geom_line()
@@ -129,7 +141,8 @@ renderCurves <- function(data, outcome, selectedAG) {
         
         mainPlot = renderPlotly({
             if (selectedAG == "All") {
-                p = ggplot(data, aes(x = Time, y = invasive.ventil, color = AgeGroup)) +
+                dataAgg = data[, .(invasive.ventil = sum(invasive.ventil)), by = c("Time","AgeGroup")]
+                p = ggplot(dataAgg, aes(x = Time, y = invasive.ventil, color = AgeGroup)) +
                     theme_classic() +
                     geom_line()
             } else if (selectedAG == "Aggregated") {
@@ -139,7 +152,8 @@ renderCurves <- function(data, outcome, selectedAG) {
                     theme_classic() +
                     geom_line()
             } else {
-                p = ggplot(data[AgeGroup == selectedAG,],
+                dataAgg = data[AgeGroup == selectedAG, .(invasive.ventil = sum(invasive.ventil)), by = c("Time","AgeGroup")]
+                p = ggplot(dataAgg,
                            aes(x = Time, y = invasive.ventil, color = AgeGroup)) +
                     theme_classic() +
                     geom_line()
@@ -148,7 +162,8 @@ renderCurves <- function(data, outcome, selectedAG) {
         })
         secondPlot = renderPlotly({
             if (selectedAG == "All") {
-                p = ggplot(data, aes(x = Time, y = non.invasive.ventil, color = AgeGroup)) +
+                dataAgg = data[, .(non.invasive.ventil = sum(non.invasive.ventil)), by = c("Time","AgeGroup")]
+                p = ggplot(dataAgg, aes(x = Time, y = non.invasive.ventil, color = AgeGroup)) +
                     theme_classic() +
                     geom_line()
             } else if (selectedAG == "Aggregated") {
@@ -158,7 +173,8 @@ renderCurves <- function(data, outcome, selectedAG) {
                     theme_classic() +
                     geom_line()
             } else {
-                p = ggplot(data[AgeGroup == selectedAG,],
+                dataAgg = data[AgeGroup == selectedAG, .(non.invasive.ventil = sum(non.invasive.ventil)), by = c("Time","AgeGroup")]
+                p = ggplot(dataAgg,
                            aes(x = Time, y = non.invasive.ventil, color = AgeGroup)) +
                     theme_classic() +
                     geom_line()
