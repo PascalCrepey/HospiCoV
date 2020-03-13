43465fc0ae859501c1ead0d5577c6958e17ce0f2
Pascal Crépey <crepey@gmail.com>
2020-03-13 14:57:33 +0100

    - get the right time and number of seeds by region

--- data-raw/CaseTimeSeries.R
+++ data-raw/CaseTimeSeries.R
@@ -8,3 +8,10 @@ names(CaseTimeSeries) <- gsub(names(CaseTimeSeries), pattern = "\\.",
                              replacement = " ")
 colnames(CaseTimeSeries)[1] = "Date"
 usethis::use_data(CaseTimeSeries, overwrite = TRUE)
+
+longCTS = melt(CaseTimeSeries, id.vars = "Date", variable.name = "Region", value.name = "Cases")
+
+
+pre_infected = longCTS[Cases >= 1, .(Date = Date[1], preInfected = Cases[1]), by = "Region"]
+
+usethis::use_data(pre_infected, overwrite = TRUE)
--- data-raw/pre-infected.R
+++
@@ -1,5 +0,0 @@
-x = unique(hospCovid$Region)
-pre_infected = data.table(Region = x,
-                          preInfected = 40)
-
-usethis::use_data(pre_infected, overwrite = TRUE)
